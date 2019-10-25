6b1112d8160621b5a6118f27f5b9a99e1cf2a875
vijay makwana <vijay.makwana@softek.co>
2019-10-25 13:48:15 +0530

    design forgot password screen and set navigation flow

--- app/src/main/java/com/tpv/android/ui/auth/forogtpassword/ForgotPasswordFragment.kt
+++ app/src/main/java/com/tpv/android/ui/auth/forogtpassword/ForgotPasswordFragment.kt
@@ -1,4 +1,4 @@
-package com.tpv.android.ui.auth.login
+package com.tpv.android.ui.auth.forogtpassword
 
 
 import android.os.Bundle
@@ -9,34 +9,27 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.livinglifetechway.k4kotlin.core.onClick
-import com.livinglifetechway.k4kotlin.core.startActivity
 import com.tpv.android.R
-import com.tpv.android.databinding.FragmentLoginBinding
-import com.tpv.android.ui.home.HomeActivity
+import com.tpv.android.databinding.FragmentForgotPasswordBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class LoginFragment : Fragment() {
-    lateinit var mBinding: FragmentLoginBinding
+class ForgotPasswordFragment : Fragment() {
 
+    private lateinit var mBinding: FragmentForgotPasswordBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_forgot_password, container, false)
         return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mBinding.btnStart.onClick {
-            context.startActivity<HomeActivity>()
-            activity?.finish()
-        }
-
-        mBinding.textForgotPassword?.onClick {
-            Navigation.findNavController(mBinding.root).navigate(R.id.action_loginFragment_to_forgotPasswordFragment)
+        mBinding.textReturnToLogin?.onClick {
+            Navigation.findNavController(mBinding.root).navigateUp()
         }
     }
 
