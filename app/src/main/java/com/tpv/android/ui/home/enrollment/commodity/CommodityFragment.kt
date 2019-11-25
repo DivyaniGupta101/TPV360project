bdcf9a7278b8f59346722e4b212b0935682e46c3
vijay makwana <vijay.makwana@softek.co>
2019-11-25 17:39:54 +0530

    rename plans to commodity in strings and file name

--- app/src/main/java/com/tpv/android/ui/home/enrollment/commodity/CommodityFragment.kt
+++ app/src/main/java/com/tpv/android/ui/home/enrollment/commodity/CommodityFragment.kt
@@ -1,4 +1,4 @@
-package com.tpv.android.ui.home.enrollment.plans
+package com.tpv.android.ui.home.enrollment.commodity
 
 
 import android.os.Bundle
@@ -12,7 +12,7 @@ import androidx.navigation.Navigation
 import com.ravikoradiya.liveadapter.LiveAdapter
 import com.tpv.android.BR
 import com.tpv.android.R
-import com.tpv.android.databinding.FragmentPlansListingBinding
+import com.tpv.android.databinding.FragmentCommodityBinding
 import com.tpv.android.databinding.ItemPlansBinding
 import com.tpv.android.model.Plans
 import com.tpv.android.ui.home.enrollment.SetEnrollViewModel
@@ -23,22 +23,22 @@ import com.tpv.android.utils.setupToolbar
 /**
  * A simple [Fragment] subclass.
  */
-class PlansListingFragment : Fragment() {
-    private lateinit var mBinding: FragmentPlansListingBinding
+class CommodityFragment : Fragment() {
+    private lateinit var mBinding: FragmentCommodityBinding
     private lateinit var mViewModel: SetEnrollViewModel
     private var mList: ArrayList<Plans> = ArrayList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_plans_listing, container, false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_commodity, container, false)
         activity?.let { mViewModel = ViewModelProviders.of(it).get(SetEnrollViewModel::class.java) }
         return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupToolbar(mBinding.toolbar, getString(R.string.plans), showBackIcon = true, showMenuIcon = true)
+        setupToolbar(mBinding.toolbar, getString(R.string.commodity), showBackIcon = true, showMenuIcon = true)
         setRecyclerView()
     }
 
@@ -53,7 +53,7 @@ class PlansListingFragment : Fragment() {
                 .map<Plans, ItemPlansBinding>(R.layout.item_plans) {
                     onClick { holder ->
                         mViewModel.planType = holder.binding.item?.planType.orEmpty()
-                        Navigation.findNavController(mBinding.root).navigateSafe(R.id.action_plansListingFragment_to_plansZipcodeFragment)
+                        Navigation.findNavController(mBinding.root).navigateSafe(R.id.action_commodityFragment_to_plansZipcodeFragment)
                     }
                 }
                 .into(mBinding.listPlans)
