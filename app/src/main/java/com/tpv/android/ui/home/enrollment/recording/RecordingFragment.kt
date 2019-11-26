3d72539c45bdd09cb6d6e5367e9f521b788de8c8
vijay makwana <vijay.makwana@softek.co>
2019-11-26 16:04:25 +0530

    solved issue of otp screen -apply input text in fragment_plans_zipcode.xml -apply padding bottom in item_plans.xml -implement style for single line and apply it

--- app/src/main/java/com/tpv/android/ui/home/enrollment/recording/RecordingFragment.kt
+++ app/src/main/java/com/tpv/android/ui/home/enrollment/recording/RecordingFragment.kt
@@ -318,6 +318,7 @@ class RecordingFragment : Fragment() {
             dialog?.dismiss()
         }
         binding?.btnYes?.onClick {
+            dialog?.dismiss()
             Navigation.findNavController(mBinding.root).navigateSafe(R.id.action_recordingFragment_to_statementFragment)
         }
 
