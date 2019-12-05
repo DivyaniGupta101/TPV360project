2b401d65a2d07ee36658562b89a632e9834c58dd
vijay makwana <vijay.makwana@softek.co>
2019-12-05 19:36:38 +0530

    bottomSheet dialog design in progress

--- app/src/main/java/com/tpv/android/ui/home/leadlisting/LeadListingFragment.kt
+++ app/src/main/java/com/tpv/android/ui/home/leadlisting/LeadListingFragment.kt
@@ -5,14 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.livinglifetechway.k4kotlin.core.onClick
 import com.ravikoradiya.liveadapter.LiveAdapter
 import com.tpv.android.BR
 import com.tpv.android.R
+import com.tpv.android.databinding.BottomSheetBinding
 import com.tpv.android.databinding.FragmentLeadListingBinding
 import com.tpv.android.databinding.ItemLeadListBinding
 import com.tpv.android.helper.setPagination
@@ -29,6 +33,7 @@ class LeadListingFragment : Fragment() {
     lateinit var mBinding: FragmentLeadListingBinding
     var toolBarTitle = ""
     lateinit var mViewModel: LeadListingViewModel
+    var status = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -47,24 +52,79 @@ class LeadListingFragment : Fragment() {
 
     private fun initialize() {
         val leadStatus = arguments?.let { LeadListingFragmentArgs.fromBundle(it) }?.item
+        status = leadStatus.orEmpty()
+        setUpToolbarTitle(leadStatus)
+        setRecyclerView(leadStatus)
 
+        mBinding.bottomStatusContainer.onClick {
+            setBottomSheet(status)
+        }
+    }
+
+    private fun setUpToolbarTitle(leadStatus: String?) {
         when (leadStatus) {
             LeadStatus.PENDING.value -> {
                 toolBarTitle = getString(R.string.pending_leads)
+                mBinding.textStatus.setText(getString(R.string.pending))
             }
             LeadStatus.VERIFIED.value -> {
                 toolBarTitle = getString(R.string.verified_leads)
+                mBinding.textStatus.setText(getString(R.string.verified))
             }
             LeadStatus.DECLINED.value -> {
                 toolBarTitle = getString(R.string.declined_leads)
+                mBinding.textStatus.setText(getString(R.string.declined))
             }
             LeadStatus.DISCONNECTED.value -> {
                 toolBarTitle = getString(R.string.disconnected_calls)
+                mBinding.textStatus.setText(getString(R.string.disconnected_calls))
             }
         }
 
         setupToolbar(mBinding.toolbar, toolBarTitle, showMenuIcon = false, showBackIcon = true)
-        setRecyclerView(leadStatus)
+    }
+
+    private fun setBottomSheet(leadStatus: String?) {
+        val binding = DataBindingUtil.inflate<BottomSheetBinding>(layoutInflater, R.layout.bottom_sheet, null, false)
+        context?.let {
+            val dialog = BottomSheetDialog(it)
+            dialog.setContentView(binding.root)
+
+            binding.radioGroup?.setOnCheckedChangeListener { group, checkedId ->
+                val radioButton = binding.radioGroup.findViewById<RadioButton>(checkedId)
+                if (radioButton.isChecked) {
+                    mBinding.textStatus.setText(radioButton.text)
+//                setUpToolbarTitle(radioButton.tag.toString())
+//                setRecyclerView(radioButton.tag.toString())
+                }
+                dialog.dismiss()
+            }
+
+            when (status) {
+                LeadStatus.PENDING.value -> {
+                    binding.radioPending.isChecked = true
+                    status = LeadStatus.PENDING.value
+                    binding.radioPending.tag = LeadStatus.PENDING.value
+                }
+                LeadStatus.VERIFIED.value -> {
+                    binding.radioVerified.isChecked = true
+                    status = LeadStatus.VERIFIED.value
+                    binding.radioVerified.tag = LeadStatus.VERIFIED.value
+                }
+                LeadStatus.DECLINED.value -> {
+                    binding.radioDeclined.isChecked = true
+                    status = LeadStatus.DECLINED.value
+                    binding.radioDeclined.tag = LeadStatus.DECLINED.value
+                }
+                LeadStatus.DISCONNECTED.value -> {
+                    binding.radioDisconnected.isChecked = true
+                    status = LeadStatus.DISCONNECTED.value
+                    binding.radioDisconnected.tag = LeadStatus.DISCONNECTED.value
+                }
+            }
+
+            dialog.show()
+        }
     }
 
     private fun setRecyclerView(leadStatus: String?) {
