8318bc41832fa7af437794f8688f67875b78f844
vijay makwana <vijay.makwana@softek.co>
2019-12-24 20:08:42 +0530

    design leadDetail screen

--- app/src/main/java/com/tpv/android/ui/home/leaddetail/LeadDetailFragment.kt
+++ app/src/main/java/com/tpv/android/ui/home/leaddetail/LeadDetailFragment.kt
@@ -7,16 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.livinglifetechway.k4kotlin.core.value
 import com.tpv.android.R
 import com.tpv.android.databinding.FragmentLeadDetailBinding
-import com.tpv.android.databinding.ItemLeadDetailBinding
-import com.tpv.android.network.error.AlertErrorHandler
-import com.tpv.android.network.resources.Resource
-import com.tpv.android.network.resources.apierror.APIError
 import com.tpv.android.utils.setupToolbar
 
 class LeadDetailFragment : Fragment() {
@@ -39,24 +32,23 @@ class LeadDetailFragment : Fragment() {
     }
 
     private fun initialize() {
-        mBinding.errorHandler = AlertErrorHandler(mBinding.root)
 
         setupToolbar(mBinding.toolbar, getString(R.string.lead_details), showBackIcon = true)
-        getLeadDetailApiCall()
+//        getLeadDetailApiCall()
     }
 
-    private fun getLeadDetailApiCall() {
-        val liveData = mViewModel.getLeadDetail(arguments?.let { LeadDetailFragmentArgs.fromBundle(it) }?.item)
-        liveData.observe(this, Observer {
-            it?.data?.forEach {
-                val binding = DataBindingUtil.inflate<ItemLeadDetailBinding>(layoutInflater, R.layout.item_lead_detail, mBinding.leadDetailContainer, true)
-                binding.textTitle.setText(it.key)
-                binding.editTitle.value = it.value.orEmpty()
-            }
-
-        })
-
-        mBinding.resource = liveData as LiveData<Resource<Any, APIError>>
-    }
+//    private fun getLeadDetailApiCall() {
+//        val liveData = mViewModel.getLeadDetail(arguments?.let { LeadDetailFragmentArgs.fromBundle(it) }?.item)
+//        liveData.observe(this, Observer {
+//            it?.data?.forEach {
+//                val binding = DataBindingUtil.inflate<ItemLeadDetailBinding>(layoutInflater, R.layout.item_lead_detail, mBinding.leadDetailContainer, true)
+//                binding.textTitle.setText(it.key)
+//                binding.editTitle.value = it.value.orEmpty()
+//            }
+//
+//        })
+//
+//        mBinding.resource = liveData as LiveData<Resource<Any, APIError>>
+//    }
 
 }
