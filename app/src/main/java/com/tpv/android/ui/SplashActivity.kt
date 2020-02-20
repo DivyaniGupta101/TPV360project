66964e25c98e3a8a47968298f6560c9e96c38039
Devanshi patel <devanshi.inexture@gmail.com>
2020-02-20 18:19:53 +0530

    handle security for screen shot

--- app/src/main/java/com/tpv/android/ui/SplashActivity.kt
+++ app/src/main/java/com/tpv/android/ui/SplashActivity.kt
@@ -1,12 +1,13 @@
 package com.tpv.android.ui
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.livinglifetechway.k4kotlin.core.startActivity
 import com.livinglifetechway.k4kotlin.databinding.setBindingView
 import com.tpv.android.R
-import com.tpv.android.ui.auth.AuthActivity
 import com.tpv.android.databinding.ActivitySplashBinding
+import com.tpv.android.ui.auth.AuthActivity
 
 
 class SplashActivity : AppCompatActivity() {
@@ -18,6 +19,11 @@ class SplashActivity : AppCompatActivity() {
         startActivity<AuthActivity>()
         finish()
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        this?.window?.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
 
