09c66c24da6c4d28108df4048c9a77de5cb469f3
Devanshi patel <devanshi.inexture@gmail.com>
2020-03-05 12:48:57 +0530

    set default geo location false

--- app/src/main/java/com/tpv/android/utils/AppConstant.kt
+++ app/src/main/java/com/tpv/android/utils/AppConstant.kt
@@ -4,7 +4,7 @@ import com.chibatching.kotpref.KotprefModel
 
 object AppConstant : KotprefModel() {
 
-    var GEO_LOCATION_ENABLE by booleanPref(true)
+    var GEO_LOCATION_ENABLE by booleanPref(false)
     var GEO_LOCATION_RADIOUS by stringPref("100")
     const val ADDRESSPICKER_KEY = "AIzaSyB5w9xL068s7yS9muLzbpQvSp6_WK1k0tE"
     const val PLACE_COUNTRY = "US"
