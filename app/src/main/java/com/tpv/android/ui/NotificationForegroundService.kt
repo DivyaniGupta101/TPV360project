f42f5fd6df7fb9ea153b24ead8621448b680ba40
Devanshi patel <devanshi.inexture@gmail.com>
2020-06-11 15:17:30 +0530

    check current location time and update location code in progress

--- app/src/main/java/com/tpv/android/ui/NotificationForegroundService.kt
+++ app/src/main/java/com/tpv/android/ui/NotificationForegroundService.kt
@@ -1,6 +1,8 @@
 package com.tpv.android.ui
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
@@ -12,15 +14,22 @@ import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavDeepLinkBuilder
 import com.google.android.gms.location.LocationCallback
+import com.livinglifetechway.k4kotlin.core.TAG
 import com.livinglifetechway.k4kotlin.core.orFalse
+import com.livinglifetechway.k4kotlin.core.orZero
+import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
 import com.tpv.android.R
 import com.tpv.android.data.AppRepository.setLocationCall
 import com.tpv.android.model.network.AgentLocationRequest
+import com.tpv.android.ui.home.TransparentActivity
+import com.tpv.android.utils.LocationHelper
 import com.tpv.android.ui.home.HomeActivity
 import com.tpv.android.utils.infoDialog
 import kotlinx.coroutines.CoroutineScope
@@ -159,7 +168,8 @@ class NotificationForegroundService : Service() {
                     locationListener)
 
         } else {
-            context.infoDialog(subTitleText = getString(R.string.msg_gps_location))
+            getLocation()
+//            context.infoDialog(subTitleText = getString(R.string.msg_gps_location))
         }
     }
 
@@ -168,5 +178,48 @@ class NotificationForegroundService : Service() {
      */
     private fun stopLocationUpdate() {
         locationManager?.removeUpdates(locationListener)
+    }
+
+    private fun getLocation() = runWithPermissions(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    ) {
+        uiScope.launch {
+            location = context?.let { LocationHelper.getLastKnownLocation(it) }
+
+
+            if (!locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER).orFalse()) {
+                context?.infoDialog(subTitleText = getString(R.string.msg_gps_location))
+            } else {
+                if (location == null) {
+                    try {
+                        startActivityForResult(context as Activity, Intent(context, TransparentActivity::class.java), TransparentActivity.REQUEST_CHECK_SETTINGS, null)
+                        location = context?.let { LocationHelper.getLastKnownLocation(it) }
+                    } catch (e: Exception) {
+                        Log.e(TAG, "getResult: ", e)
+                    }
+                } else {
+                    if (location?.time.orZero() >= 1000) {
+
+                        for (i in 1..3) {
+                            if (location?.time.orZero() < 1000) {
+                                break
+                            } else {
+                                try {
+                                    startActivityForResult(context as Activity, Intent(context, TransparentActivity::class.java), TransparentActivity.REQUEST_CHECK_SETTINGS, null)
+                                    location = context?.let { LocationHelper.getLastKnownLocation(it) }
+                                } catch (e: Exception) {
+                                    Log.e(TAG, "getResult: ", e)
+                                }
+                            }
+                        }
+                        if (location?.time.orZero() >= 1000) {
+                            context.infoDialog(subTitleText = getString(R.string.msg_unable_detect_location))
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
