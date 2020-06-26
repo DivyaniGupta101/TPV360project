aa71ad87040558fd223d90523d8099b0cda3670e
vijay makwana <vijay.makwana@softek.co>
2020-06-26 15:38:49 +0530

    - Added search item in pref - Show search item in search screen

--- app/src/main/java/com/tpv/android/helper/Pref.kt
+++ app/src/main/java/com/tpv/android/helper/Pref.kt
@@ -3,9 +3,15 @@ package com.tpv.android.helper
 import com.chibatching.kotpref.KotprefModel
 import com.chibatching.kotpref.gsonpref.gsonNullablePref
 import com.tpv.android.model.network.UserDetail
+import java.util.*
+import kotlin.collections.HashSet
 
 object Pref : KotprefModel() {
 
     var token by nullableStringPref()
     var user by gsonNullablePref<UserDetail>()
+    val searchText by stringSetPref{
+        val set = HashSet<String>()
+        return@stringSetPref set
+    }
 }
--- app/src/main/java/com/tpv/android/ui/client/ui/SearchActivity.kt
+++ app/src/main/java/com/tpv/android/ui/client/ui/SearchActivity.kt
@@ -8,8 +8,12 @@ import com.livinglifetechway.k4kotlin.core.hideKeyboard
 import com.livinglifetechway.k4kotlin.core.onClick
 import com.livinglifetechway.k4kotlin.core.value
 import com.livinglifetechway.k4kotlin.databinding.setBindingView
+import com.ravikoradiya.liveadapter.LiveAdapter
+import com.tpv.android.BR
 import com.tpv.android.R
 import com.tpv.android.databinding.ActivitySearchBinding
+import com.tpv.android.databinding.ItemSearchBinding
+import com.tpv.android.helper.Pref
 import com.tpv.android.ui.client.ui.reports.reportslisting.ClientReportsListingFragment
 
 
@@ -21,21 +25,34 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = setBindingView(R.layout.activity_search)
 
+        LiveAdapter(Pref.searchText.toList(), BR.item)
+                .map<String, ItemSearchBinding>(R.layout.item_search) {
+                    onClick {
+                        passValueToFragment(it.binding.item.orEmpty())
+                    }
+                }
+                .into(mBinding.rvSearch)
+
         mBinding.imageToolbarBack.onClick {
             finish()
         }
 
         mBinding.editSearch.setOnEditorActionListener { textView, actionId, keyEvent ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                hideKeyboard()
-                val intent = Intent()
-                intent.putExtra(ClientReportsListingFragment.EXTRA_KEY_SEARCH, mBinding.editSearch.value)
-                setResult(ClientReportsListingFragment.RESULT_CODE, intent) // You can also send result without any data using setResult(int resultCode)
-                finish()
+                Pref.searchText.add(mBinding.editSearch.value)
+                passValueToFragment(mBinding.editSearch.value)
                 return@setOnEditorActionListener true
             }
             false
         }
 
+    }
+
+    private fun passValueToFragment(value: String) {
+        hideKeyboard()
+        val intent = Intent()
+        intent.putExtra(ClientReportsListingFragment.EXTRA_KEY_SEARCH, value)
+        setResult(ClientReportsListingFragment.RESULT_CODE, intent) // You can also send result without any data using setResult(int resultCode)
+        finish()
     }
 }
