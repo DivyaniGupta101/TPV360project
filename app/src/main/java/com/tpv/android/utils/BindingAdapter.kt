b15d94bb10bfcab90e1ff90a8765c1b9d47c1fa8
Devanshi patel <devanshi.inexture@gmail.com>
2020-08-24 17:58:30 +0530

    remove space and value

--- app/src/main/java/com/tpv/android/utils/BindingAdapter.kt
+++ app/src/main/java/com/tpv/android/utils/BindingAdapter.kt
@@ -293,7 +293,7 @@ object BindingAdapter {
 
     fun setBracketText(editText: EditText, primaryValue: String, secondaryText: String, secondaryValue: String) {
 
-        editText.value = "$primaryValue ( $secondaryText : $secondaryValue)"
+        editText.value = "$primaryValue ($secondaryText: $secondaryValue)"
     }
 
 
